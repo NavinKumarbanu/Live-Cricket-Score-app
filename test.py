8df1f56cc601d07435596963dbaba4bce412c7e8
Navin Kumar <168159694+NavinKumarbanu@users.noreply.github.com>
2025-07-27 10:53:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import requests
 
-url = "https://cricbuzz-cricket.p.rapidapi.com/matches/v1/recent"
+url = 'https://cricbuzz-cricket.p.rapidapi.com/matches/v1/recent'
 
 headers = {
-        "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
-        "X-RapidAPI-Key": "Replace with your RapidAPI key"  # Replace with your RapidAPI key
-    }
+    "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
+    "X-RapidAPI-Key": "327a4f3817msh0e716c198181c52p17c0ffjsn0e7e53feb9b3"  # Replace with your key
+}
+
 response = requests.get(url, headers=headers)
-print(response.json())
-  
+
+# Check status
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Failed to fetch data. Status code: {response.status_code}")
