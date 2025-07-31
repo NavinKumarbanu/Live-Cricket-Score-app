47dc857859ecec217c826639891793e52392c16a
Navin Kumar <168159694+NavinKumarbanu@users.noreply.github.com>
2025-07-31 17:03:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ def fetch_cricket_scores():
 
     headers = {
         "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
-        "X-RapidAPI-Key": "327a4f3817msh0e716c198181c52p17c0ffjsn0e7e53feb9b3"  # Replace this with your actual API key
+        "X-RapidAPI-Key": "327a4f3817msh0e716c198181c52p17c0ffjsn0e7e53feb9b3"  # Replace with your actual key
     }
 
     response = requests.get(url, headers=headers)
-    
+
     try:
         data = response.json()
     except ValueError:
@@ -46,7 +46,7 @@ def fetch_cricket_scores():
 @app.route('/')
 def index():
     cricket_scores = fetch_cricket_scores()
-    return render_template('index.html', matches=cricket_scores)
+    return render_template('index.html', cricket_scores=cricket_scores)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
