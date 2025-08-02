0618a447213205fe6e62322c46286f9a29f4eee3
Navin Kumar <168159694+NavinKumarbanu@users.noreply.github.com>
2025-08-02 11:21:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ url = "https://cricbuzz-cricket.p.rapidapi.com/matches/v1/recent"
 
 headers = {
     "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
-    "X-RapidAPI-Key": "327a4f3817msh0e716c198181c52p17c0ffjsn0e7e53feb9b3"
+    "X-RapidAPI-Key": "f33c97bc66mshe003db879159c21p123f11jsn4c9a3f5a812d"
 }
 
 response = requests.get(url, headers=headers)
