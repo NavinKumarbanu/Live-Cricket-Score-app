4e4d6ca5c3b082f5942a64619a8e0d2b03671399
Navin Kumar <168159694+NavinKumarbanu@users.noreply.github.com>
2025-08-02 11:22:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def fetch_cricket_scores():
 
     headers = {
         "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
-        "X-RapidAPI-Key": "327a4f3817msh0e716c198181c52p17c0ffjsn0e7e53feb9b3"  
+        "X-RapidAPI-Key": "f33c97bc66mshe003db879159c21p123f11jsn4c9a3f5a812d"  
     }
     response = requests.get(url, headers=headers)
     data = response.json()
@@ -37,7 +37,7 @@ def fetch_upcoming_matches():
 
     headers = {
         "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
-        "X-RapidAPI-Key": "327a4f3817msh0e716c198181c52p17c0ffjsn0e7e53feb9b3"  # Replace with your RapidAPI key
+        "X-RapidAPI-Key": "f33c97bc66mshe003db879159c21p123f11jsn4c9a3f5a812d"  # Replace with your RapidAPI key
     }
     #
     response = requests.get(url, headers=headers)
